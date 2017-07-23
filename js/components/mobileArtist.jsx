418ace7e2972a87329dce154e8f65586710be4d3
AleksandraKaminska <kaminska97@live.com>
2017-07-23 16:37:50 +0200

    initial commit

--- js/components/mobileArtist.jsx
+++ js/components/mobileArtist.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ArtistInfo from './artistInfo.jsx';
+import Concerts from './concerts.jsx';
 
 class MobileArtist extends React.Component {
   render() {
     return <div className='mainMiddle'>
 					<ArtistInfo />
+          <Concerts />
 				</div>
   }
 }
