09daf1f4b3159173aab110f58ff0b8544f9e917b
AleksandraKaminska <kaminska97@live.com>
2017-08-04 20:57:03 +0200

    initial commit

--- js/components/nav.jsx
+++ js/components/nav.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 class Nav extends React.Component {
   render() {
     return <nav className='nav'>
-      <a href='#playlists' aria-expanded="false">main</a>
-      <a href='#similar' aria-expanded="false">similar artists</a>
-      <a href='#albums' aria-expanded="false">albums</a>
+      <a id='main_nav' href='#playlists' aria-expanded="false">main</a>
+      <a id='similar_nav' href='#similar' aria-expanded="false">similar artists</a>
+      <a id='albums_nav' href='#albums' aria-expanded="false">albums</a>
     </nav>
   }
 }
--- js/out.js
+++ js/out.js
@@ -16480,17 +16480,17 @@ var Nav = function (_React$Component) {
         { className: 'nav' },
         _react2.default.createElement(
           'a',
-          { href: '#playlists', 'aria-expanded': 'false' },
+          { id: 'main_nav', href: '#playlists', 'aria-expanded': 'false' },
           'main'
         ),
         _react2.default.createElement(
           'a',
-          { href: '#similar', 'aria-expanded': 'false' },
+          { id: 'similar_nav', href: '#similar', 'aria-expanded': 'false' },
           'similar artists'
         ),
         _react2.default.createElement(
           'a',
-          { href: '#albums', 'aria-expanded': 'false' },
+          { id: 'albums_nav', href: '#albums', 'aria-expanded': 'false' },
           'albums'
         )
       );
