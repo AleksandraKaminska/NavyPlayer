c5429bc48f3c004dea8dce8390b7475f7308fe2c
AleksandraKaminska <aleksandra-kaminska@outlook.com>
2017-12-16 14:14:46 +0100

    playlists

--- js/out.js
+++ js/out.js
@@ -14740,7 +14740,7 @@ const reducers = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0_redux__["b" /
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony default export */ __webpack_exports__["a"] = (function (state = 3773396182, action) {
+/* harmony default export */ __webpack_exports__["a"] = (function (state = 3773392382, action) {
     switch (action.type) {
         case 'CHANGE_PLAYLIST':
             return action.chosenPlaylist;
@@ -15504,7 +15504,7 @@ var ChoosePlaylists = function (_React$Component) {
             }
         };
 
-        _this.playlists = [3773396182, 3773404202, 3773392382, 2506704664, 3850597162, 1057779131, 1266972311, 715214945, 3570967222, 2097558104, 1282483245, 2734448044, 1282495565, 1728093421, 1306931615, 2178064502, 1927928822, 1977689462, 1964028802, 1677006641, 1290756705, 1154685481, 515157085, 1386209585, 1182263621, 2265794682, 1661692771, 2558770224, 975986691];
+        _this.playlists = [3773392382, 2506704664, 3850597162, 1282483245, 3155776842, 3188520162, 1242578051, 1057779131, 1653487581, 715214945, 3570967222, 2097558104, 2734448044, 1282495565, 1728093421, 1306931615, 2178064502, 1977689462, 1964028802, 1677006641, 1290756705, 1386209585, 1182263621, 2265794682, 1661692771, 2558770224, 975986691];
         return _this;
     }
 
--- js/reducers/reducer-chosen-playlist.js
+++ js/reducers/reducer-chosen-playlist.js
@@ -1,4 +1,4 @@
-export default function (state = 3773396182, action) {
+export default function (state = 3773392382, action) {
     switch (action.type) {
         case 'CHANGE_PLAYLIST':
             return action.chosenPlaylist;
