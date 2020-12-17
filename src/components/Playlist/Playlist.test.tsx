c18ea32b8798a65af6864ebcde40cd4812b3dafa
AleksandraKaminska <aleksandra-kaminska@outlook.com>
2020-12-17 16:35:27 +0100

    add playlist tests

---
+++ src/components/Playlist/Playlist.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Playlist from './Playlist'
+import { PlaylistType, UserType } from '../../types/deezerData'
+
+const user: UserType = {
+  id: 123,
+  name: 'username',
+  tracklist: 'tracklist-url',
+  type: 'user'
+}
+
+const playlist: PlaylistType = {
+  id: 1,
+  checksum: 'checksum',
+  creation_date: '2020',
+  link: 'link-url',
+  md5_image: 'md5-url',
+  nb_tracks: 123,
+  picture: 'picture-url',
+  picture_big: 'picture-big-url',
+  picture_medium: 'picture-medium-url',
+  picture_small: 'picture-small-url',
+  picture_xl: 'picture-xl-url',
+  public: true,
+  title: 'title',
+  tracklist: 'tracklist-url',
+  type: 'playlist',
+  user
+}
+
+test('renders correctly', () => {
+  render(<Playlist data={playlist} />, { wrapper: BrowserRouter })
+  expect(screen.getByAltText(/title/i)).toBeInTheDocument()
+  expect(screen.getByAltText(/title/i)).toHaveAttribute('src', 'picture-medium-url')
+  expect(screen.getByText(/title/i)).toBeInTheDocument()
+  expect(screen.getByText(/123 tracks/i)).toBeInTheDocument()
+})
+
+test('does not render if data props not passed', () => {
+  render(<Playlist />, { wrapper: BrowserRouter })
+  expect(screen.queryByAltText(/title/i)).not.toBeInTheDocument()
+  expect(screen.queryByText(/title/i)).not.toBeInTheDocument()
+  expect(screen.queryByText(/123 tracks/i)).not.toBeInTheDocument()
+})
