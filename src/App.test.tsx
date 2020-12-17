09a706b2d08806c335ac5b4313625bfe4ecdf941
AleksandraKaminska <aleksandra-kaminska@outlook.com>
2020-12-17 02:58:12 +0100

    Remove an unused test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-// test('renders learn react link', () => {
-//   render(<App />)
-//   const linkElement = screen.getByText(/learn react/i)
-//   expect(linkElement).toBeInTheDocument()
-// })
+test('renders deezer logo', () => {
+  render(<App />)
+  const deezerText = screen.getByText(/Powered by Deezer/i)
+  expect(deezerText).toBeInTheDocument()
+})
