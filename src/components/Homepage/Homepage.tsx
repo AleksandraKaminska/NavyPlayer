e41e73fa8936b7bc93c0c5c368082394fe2d20f8
AleksandraKaminska <aleksandra-kaminska@outlook.com>
2020-12-20 00:37:04 +0100

    update homepage

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -3,6 +3,7 @@ import fetchJsonp from 'fetch-jsonp'
 import { Spin } from 'antd'
 import { LoadingOutlined } from '@ant-design/icons'
 import { StateContext, DispatchContext } from '../../context/Context'
+import Artist from '../Artist/Artist'
 import Playlist from '../Playlist/Playlist'
 import Carousel from '../Carousel/Carousel'
 import Album from '../Album/Album'
@@ -19,6 +20,7 @@ const Homepage = () => {
   const loadMore = async () => {
     const resp = await (await fetchJsonp(`https://api.deezer.com/chart?index=${index}&limit=20&output=jsonp`)).json()
     const data = await resp
+    console.log(data)
     dispatch({
       type: 'TOP_CHART',
       payload: data
@@ -48,6 +50,16 @@ const Homepage = () => {
         onSlideChange={onSlideChange}
       >
         <Playlist />
+      </Carousel>
+      <Carousel
+        className="artists"
+        title="Popular artists"
+        slider
+        data={state.topChart?.artists?.data}
+        onSlideChange={onSlideChange}
+        rounded
+      >
+        <Artist />
       </Carousel>
       <Carousel
         className="albums"
